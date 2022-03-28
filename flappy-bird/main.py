78d8186e663aa2ac46afcc61da521fb0b6fd589c
emamyari <emamyari@yahoo.com>
2022-03-28 19:30:28 +0430

    add bird movement

--- flappy-bird/main.py
+++ flappy-bird/main.py
@@ -6,9 +6,14 @@ pygame.init()
 fps = 90
 width = 400
 height = 800
-floorX=0
+floorX = 0
+birdMove = 0
+grav = 0.2
 bg = pygame.transform.scale(pygame.image.load("assets/img/bg2.png"), (width, height))
-floorImage = pygame.transform.scale(pygame.image.load('assets/img/floor.png'),(width, 200))
+floorImage = pygame.transform.scale(pygame.image.load('assets/img/floor.png'), (width, 200))
+bird = pygame.transform.scale2x(pygame.image.load("assets/img/red_bird_mid_flap.png"))
+
+birdRect = bird.get_rect(center=(50, 300))
 
 win = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
@@ -19,13 +24,24 @@ while run:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    floorX -=1
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE:
+                birdMove=0
+                birdMove -=5
+
+    print(birdMove)
+
+    floorX -= 1
+    birdMove+=grav
+    birdRect.centery+=birdMove
 
     win.blit(bg, (0, 0))
+
+    win.blit(bird, birdRect)
     win.blit(floorImage, (floorX, 650))
-    win.blit(floorImage, (floorX+width, 650))
-    if floorX<=-400:
-        floorX=0
+    win.blit(floorImage, (floorX + width, 650))
+    if floorX <= -400:
+        floorX = 0
 
     pygame.display.update()
     clock.tick(fps)
