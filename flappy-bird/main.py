7636c7ba05ba4e61ee4b4660da52774588a137bd
sadra <sadra.akhtari87@gmail.com>
2022-03-28 22:26:25 -0700

    add bird movement

--- flappy-bird/main.py
+++ flappy-bird/main.py
@@ -3,45 +3,50 @@ import sys
 import pygame
 
 pygame.init()
-fps = 90
-width = 400
-height = 800
-floorX = 0
+
+width = 500
+height = 710
+fps = 60
+floorx = 0
 birdMove = 0
 grav = 0.2
 bg = pygame.transform.scale(pygame.image.load("assets/img/bg2.png"), (width, height))
-floorImage = pygame.transform.scale(pygame.image.load('assets/img/floor.png'), (width, 200))
-bird = pygame.transform.scale2x(pygame.image.load("assets/img/red_bird_mid_flap.png"))
-
-birdRect = bird.get_rect(center=(50, 300))
-
-win = pygame.display.set_mode((width, height))
+floorimage = pygame.transform.scale(pygame.image.load("assets/img/floor.png"), (width, 225))
+bird = pygame.transform.scale(pygame.image.load("assets/img/red_bird_mid_flap.png"), (50, 35))
+birdDown=pygame.transform.scale(pygame.image.load("assets/img/red_bird_down_flap.png"), (50, 35))
+birdUp=pygame.transform.scale(pygame.image.load("assets/img/red_bird_up_flap.png"), (50, 35))
+birdRect = bird.get_rect(center=(50, 275 ))
+win = pygame.display.set_mode([width, height])
+pygame.display.set_caption("Flappy bird")
 clock = pygame.time.Clock()
-
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_SPACE:
-                birdMove=0
-                birdMove -=5
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                birdMove = 0
+                birdMove -= 5
 
-    print(birdMove)
+    floorx -= 1
+    birdMove += grav
 
-    floorX -= 1
-    birdMove+=grav
-    birdRect.centery+=birdMove
-
+    birdRect.centery += birdMove
     win.blit(bg, (0, 0))
-
     win.blit(bird, birdRect)
-    win.blit(floorImage, (floorX, 650))
-    win.blit(floorImage, (floorX + width, 650))
-    if floorX <= -400:
-        floorX = 0
+    if birdMove<0:
+        win.blit(birdUp, birdRect)
+    if birdMove>0:
+        win.blit(birdDown, birdRect)
+    win.blit(floorimage, (floorx, 550))
+    win.blit(floorimage, (floorx + width, 550))
+
+    if floorx <= -500:
+        floorx = 0
 
     pygame.display.update()
     clock.tick(fps)
+
+pygame.QUIT()
