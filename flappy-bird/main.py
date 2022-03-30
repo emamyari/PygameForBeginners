0306bf1a79c706912bb06ce2fd667729f5c9b9a8
emamyari <emamyari@yahoo.com>
2022-03-30 19:18:23 +0430

    score handling

--- flappy-bird/main.py
+++ flappy-bird/main.py
@@ -4,25 +4,41 @@ import pygame
 import random
 
 pygame.init()
+birdEvent = pygame.USEREVENT
+pygame.time.set_timer(birdEvent, 100)
+pipeEvent = pygame.USEREVENT+1
+pygame.time.set_timer(pipeEvent, 1000)
+
+
 fps = 90
 width = 400
 height = 800
 floorX = 0
 birdMove = 0
 grav = 0.2
+pipeList = []
 game_status = True
+score=0
+activeScore=True
+
+
 bg = pygame.transform.scale(pygame.image.load("assets/img/bg2.png"), (width, height))
 floorImage = pygame.transform.scale(pygame.image.load('assets/img/floor.png'), (width, 200))
-# bird = pygame.transform.scale2x(pygame.image.load("assets/img/red_bird_mid_flap.png"))
-bird =  pygame.image.load("assets/img/red_bird_mid_flap.png")
 pipe = pygame.image.load("assets/img/pipe_green.png")
+
+bird_up = pygame.image.load("assets/img/red_bird_up_flap.png")
+bird_mid = pygame.image.load("assets/img/red_bird_mid_flap.png")
+bird_down = pygame.image.load("assets/img/red_bird_down_flap.png")
+
+font=pygame.font.Font("assets/font/Flappy.TTF",40)
+
+bird_list = [bird_up, bird_mid, bird_down]
+bird_index = 0
+bird = bird_list[bird_index]
+
 birdRect = bird.get_rect(center=(50, 300))
-pipeList = []
 win = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-
-pipeEvent = pygame.USEREVENT
-pygame.time.set_timer(pipeEvent, 1000)
 
 
 def generatePipeRect():
@@ -35,6 +51,8 @@ def generatePipeRect():
 def movePipe(pipes):
     for pipeRect in pipes:
         pipeRect.centerx -= 3
+        if pipeRect.right<0:
+            pipes.remove(pipeRect)
     return pipes
 
 
@@ -55,6 +73,26 @@ def checkCollision(pipes):
             return False
     return True
 
+
+def birdAnime():
+    newBird = bird_list[bird_index]
+    newBirdRect = newBird.get_rect(center=(50, birdRect.centery))
+    return newBird, newBirdRect
+
+def displayScore():
+    text=font.render(str(score),False,(255,255,255))
+    textRect=text.get_rect(center=(185,100))
+    win.blit(text,textRect)
+
+def updateScore():
+    global score,activeScore
+    for pipeRect in pipeList:
+        if 40<pipeRect.centerx<60 and activeScore:
+            score+=1
+            activeScore=False
+        if pipeRect.centerx<40:
+            activeScore=True
+
 run = True
 while run:
     for event in pygame.event.get():
@@ -65,14 +103,21 @@ while run:
             if event.key == pygame.K_SPACE:
                 birdMove = 0
                 birdMove -= 5
-            if event.key== pygame.K_t:
-                game_status=True
-                birdMove=0
+            if event.key == pygame.K_t:
+                game_status = True
+                birdMove = 0
                 pipeList.clear()
-                birdRect.center=(50,300)
+                birdRect.center = (50, 300)
 
         if event.type == pipeEvent:
             pipeList.extend(generatePipeRect())
+        if event.type == birdEvent:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+            bird, birdRect = birdAnime()
+
     floorX -= 1
     birdMove += grav
     birdRect.centery += birdMove
@@ -85,6 +130,9 @@ while run:
         win.blit(bird, birdRect)
         win.blit(floorImage, (floorX, 650))
         win.blit(floorImage, (floorX + width, 650))
+        displayScore()
+        updateScore()
+
 
     if floorX <= -400:
         floorX = 0
