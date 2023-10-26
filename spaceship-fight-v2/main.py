937aa6ea4dbde441173f5279c784ccad5b0bfc08
emamyari <emamyari@yahoo.com>
2023-10-26 17:18:20 +0330

    main file was changed.

--- spaceship-fight-v2/main.py
+++ spaceship-fight-v2/main.py
@@ -1,9 +1,13 @@
 from builtins import range
+import time
 
 import pygame
+pygame.init()
+
 width=900
 hight=500
 fps=60
+
 pygame.mixer.init()
 win=pygame.display.set_mode((width,hight))
 sRed=pygame.image.load('aks/spaceship_red.png')
@@ -17,6 +21,8 @@ bgp = pygame.transform.scale(bg, (width, hight))
 bomb=pygame.mixer.Sound('aks/Grenade+1.mp3')
 tir=pygame.mixer.Sound('aks/Gun+Silencer.mp3')
 
+font = pygame.font.Font("aks/Flappy.TTF", 40)
+
 red = pygame.Rect(700, 300, 55, 40)
 yellow = pygame.Rect(100, 300, 55, 40)
 clock = pygame.time.Clock()
@@ -25,6 +31,9 @@ gy=pygame.Rect(920, -20, 10, 5)
 gr=pygame.Rect(-20, -20, 10, 5)
 
 a=1
+joonRed=2
+joonYellow=2
+
 while a>0:
     clock.tick(fps)
     for event in pygame.event.get():
@@ -37,6 +46,15 @@ while a>0:
     win.blit(spYellow, (yellow.x, yellow.y))
     pygame.draw.rect(win,(255,0,0),gy)
     pygame.draw.rect(win,(255,255,255),gr)
+
+    text = font.render("HP: " + str(joonRed), True, (255, 255, 255))
+    textRect = text.get_rect(center=(800, 50))
+    win.blit(text, textRect)
+
+    text = font.render("HP: " + str(joonYellow), True, (255, 255, 255))
+    textRect = text.get_rect(center=(100, 50))
+    win.blit(text, textRect)
+
     pygame.display.update()
 
     key_pressed=pygame.key.get_pressed()
@@ -77,7 +95,25 @@ while a>0:
     if gy.colliderect(red):
         bomb.play()
         gy.x=950
+        joonRed=joonRed-1
+        if joonRed==-1:
+            a=0
+            text = font.render("yellow win..." , True, (255, 255, 255))
+            textRect = text.get_rect(center=(400, 250))
+            win.blit(text, textRect)
+            pygame.display.update()
+
+            time.sleep(3)
 
     if gr.colliderect(yellow):
         bomb.play()
         gr.x=-20
+        joonYellow-=1
+        if joonYellow==-1:
+            a=0
+            text = font.render("red win..." , True, (255, 255, 255))
+            textRect = text.get_rect(center=(400, 250))
+            win.blit(text, textRect)
+            pygame.display.update()
+
+            time.sleep(3)
